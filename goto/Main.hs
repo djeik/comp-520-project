fb41caf586dd2844ec34384d22f9431625e9ceff
Jacob Errington <git@mail.jerrington.me>
2016-01-19 15:08:00 -0500

    improve the goto binary to simplify testing
    The goto executable now parses an expression from standard in and uses
    the pretty-show library to print the result. This makes debugging a bit
    easier when tests fail.

--- goto/Main.hs
+++ goto/Main.hs
@@ -1,4 +1,17 @@
 module Main where
 
+import Language.GoLite
+
+import Text.Megaparsec
+import Text.Megaparsec.String
+import Text.Show.Pretty
+
+parseOnly :: Parser a -> String -> String -> Either ParseError a
+parseOnly m = parse (m <* eof)
+
 main :: IO ()
-main = putStrLn "Hello world!"
+main = do
+    stdin <- getContents
+    putStrLn $ case parseOnly expr "stdin" stdin of
+        Left e -> ppShow e
+        Right r -> ppShow r
