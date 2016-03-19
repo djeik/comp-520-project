9cba79324d6547094a3cfb3fd9d8e2ae38c537f9
Fred Lafrance <lafrancef@gmail.com>
2016-03-18 20:37:45 -0400

    Rewrote expression weeding to use a cata. Also close #44

--- libgoto/Language/GoLite/Weeder/Expr.hs
+++ libgoto/Language/GoLite/Weeder/Expr.hs
@@ -9,6 +9,8 @@ Stability   : experimental
 "Weeder" traversal definition for expressions.
 -}
 
+{-# LANGUAGE TypeFamilies #-}
+
 module Language.GoLite.Weeder.Expr
 ( weedExpr
 ) where
@@ -16,71 +18,43 @@ module Language.GoLite.Weeder.Expr
 import Language.GoLite.Weeder.Core
 import Language.GoLite.Weeder.TypeLit
 
+import Data.Functor.Foldable ( cata )
+
 {-| Weeds an expression and its sub-expressions.
 
-    * If a call has a type, the callee must be @make@. (Note that the converse,
-        if the callee is @make@, then the call must have a type, cannot be
-        reliably checked without a symbol table.)
     * The blank identifier may not be used in an expression. Cases where this is
       in fact allowed should deal with the blank identifier separately instead
       of calling "weedExpr".
+    * The reference, dereference and receive operators are unsupported.
+    * Types in an expression are weeeded.
 -}
 weedExpr :: SrcAnnExpr -> Weeder ()
--- Binary operator: weed both sides.
-weedExpr (Fix (Ann _ (BinaryOp _ l r))) = do
-    weedExpr l
-    weedExpr r
+weedExpr = cata phi where
 
--- Unary operator: weed the operand.
-weedExpr (Fix (Ann _ (UnaryOp _ e))) = weedExpr e
+    -- Unary operator: weed the operator.
+    phi (Ann a (UnaryOp o _)) =
+        let o' = bare o in
+        when (o' == Dereference || o' == Reference || o' == Receive)
+            (reportError (a, "Unsupported operator"))
 
--- Conversion: weed the type and operand.
-weedExpr (Fix (Ann _ (Conversion ty e))) = do
-    weedType ty
-    weedExpr e
+    -- Conversion: weed the type
+    phi (Ann _ (Conversion ty _)) = weedType ty
 
--- Selector: check that the identifier is not the blank identifier, then weed
--- the operand.
-weedExpr (Fix (Ann _ (Selector e i))) = do
-    errorOnBlankIdentifier i "cannot refer to blank field"
-    weedExpr e
+    -- Selector: check that the identifier is not the blank identifier
+    phi (Ann _ (Selector _ i)) =
+        errorOnBlankIdentifier i "cannot refer to blank field"
 
--- Index: weed the indexer and indexee.
-weedExpr (Fix (Ann _ (Index e v))) = do
-    weedExpr e
-    weedExpr v
+    -- Type assertion: unsupported
+    phi (Ann a (TypeAssertion _ _)) =
+        reportError (a, "Type assertions are unsupported")
 
--- Slice: weed the expression and all slice parts. We don't check that the
--- combinations of Maybes is valid since the parser should reject it.
-weedExpr (Fix (Ann _ (Slice e l h b))) = do
-    weedExpr e
-    void $ pure (weedExpr <$> l)
-    void $ pure (weedExpr <$> h)
-    void $ pure (weedExpr <$> b)
+    -- Call: weed the type
+    phi (Ann _ (Call _ ty _)) = do
+        void $ pure (weedType <$> ty)
 
--- Type assertion: weed the expression and type being asserted.
-weedExpr (Fix (Ann _ (TypeAssertion e ty))) = do
-    weedExpr e
-    weedType ty
+    -- Variable: not the blank identifier.
+    phi (Ann _ (Variable v)) =
+        errorOnBlankIdentifier v "_ cannot be used as a value"
 
--- Call: check that if a type is present, make is used. Then, weed the callee
--- and params expressions, as well as the type.
-weedExpr (Fix (Ann _ (Call callee ty params))) = do
-    -- We can check for this here because assigning built-ins is illegal, so
-    -- if we have a type in the call, we better be calling something called
-    -- "make". This won't catch cases where something else shadows the "make"
-    -- built-in but those can be verified later.
-    when (isJust ty && not ("make" `isIdAsOperand` callee))
-        (let Just ty' = ty in
-        reportError (topAnn ty', "type is not an expression"))
-
-    weedExpr callee
-    void $ pure (weedType <$> ty)
-    void $ mapM weedExpr params
-
--- Literals: literally nothing.
-weedExpr (Fix (Ann _ (Literal _))) = pure ()
-
--- Variable: not the blank identifier.
-weedExpr (Fix (Ann _ (Variable v))) =
-    errorOnBlankIdentifier v "_ cannot be used as a value"
+    -- Others: nothing
+    phi _ = pure ()
