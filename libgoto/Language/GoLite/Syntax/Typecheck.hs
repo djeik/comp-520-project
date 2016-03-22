9781bd84cfbf2403875424a07c855daee99f6861
Jacob Errington <git@mail.jerrington.me>
2016-03-21 21:26:34 -0400

    fix #74

--- libgoto/Language/GoLite/Syntax/Typecheck.hs
+++ libgoto/Language/GoLite/Syntax/Typecheck.hs
@@ -125,7 +125,7 @@ instance Pretty TySrcAnnLiteral where
         IntLit x -> pretty x <+> pretty (Comment ty)
         FloatLit x -> pretty x <+> pretty (Comment ty)
         RuneLit x -> pretty x <+> pretty (Comment ty)
-        StringLit x -> doubleQuotes (text x) <+> pretty (Comment ty)
+        StringLit x -> doubleQuotes (text $ show x) <+> pretty (Comment ty)
 
 instance Pretty TySrcAnnExpr where
     pretty = (\(_, _, x) -> x) . cata f where
