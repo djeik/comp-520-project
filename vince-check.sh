606bc485120721bd9264fd1285ddb33305942d8c
Jacob Errington <git@mail.jerrington.me>
2016-03-22 20:14:46 -0400

    create script for cross-referencing vince's compiler

---
+++ vince-check.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Checks programs against golitec
+
+ssh mimi.cs.mcgill.ca 'mkdir -p goto-testing'
+
+rsync -r programs mimi.cs.mcgill.ca:goto-testing/
+
+ssh mimi.cs.mcgill.ca "bash" <<'EOF'
+#!/bin/bash
+
+set -e
+
+GOLITEC="/home/course/cs520/golitec"
+OUT="goto-testing/out"
+
+find goto-testing/programs/valid -type f |
+while read line ; do
+    "$GOLITEC" parse "$line" > "$OUT" 2>&1 || cat "$OUT"
+done
+
+find goto-testing/programs/invalid -type f |
+while read line ; do
+    "$GOLITEC" parse "$line" > "$OUT" 2>&1 && (echo "$line" ; cat "$OUT")
+done
+
+find goto-testing/programs/valid-type -type f |
+while read line ; do
+    "$GOLITEC" typecheck "$line" > "$OUT" 2>&1 || cat "$OUT"
+done
+
+find goto-testing/programs/invalid-type -type f |
+while read line ; do
+    "$GOLITEC" typecheck "$line" > "$OUT" 2>&1 && (echo "$line" ; cat "$OUT")
+done
+EOF
+
